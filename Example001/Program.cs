2de5fefd58b5fba95a85631540868f011052a9f1
Nikita Pletnev <betpezeh1@yahoo.com>
2022-09-05 13:11:06 +0500

    add Example 002

--- Example001/Program.cs
+++ Example001/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.Clear();
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.Clear();
 int num = new Random().Next(100, 1000);
 
 Console.WriteLine(num);
---
+++ Example002/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая выводит третью цифру слева заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите число А: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+    if (a < 100)
+{
+    Console.WriteLine("В данном числе нет третьей цифры");
+}
+else if (a > 100)
+    {
+        while (a >= 1000) 
+            a /= 10;
+            int d = a % 10;   
+         Console.WriteLine(d);
+    }
