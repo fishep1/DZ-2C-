a373f4a731546418e12960738c8385902a3259e1
Nikita Pletnev <betpezeh1@yahoo.com>
2022-09-05 13:29:13 +0500

    add Example 003

---
+++ Example003/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("введите номер дня недели ");
+
+int a = Convert.ToInt32(Console.ReadLine()); // конвертируем строку в число
+
+string dayOfWeek = ("день недели");
+if (a == 1)
+{
+dayOfWeek = ("Понедельник");
+}
+if (a == 2)
+{
+dayOfWeek = ("вторник");
+}
+if (a == 3)
+{
+dayOfWeek = ("среда");
+}
+if (a == 4)
+{
+dayOfWeek = ("четверг");
+}
+if (a == 5)
+{
+dayOfWeek = ("Пятница");
+}
+if (a == 6)
+{
+dayOfWeek = ("Суббота");
+}
+if (a == 7)
+{
+dayOfWeek = ("воскресенье");
+}
+Console.WriteLine(dayOfWeek);
